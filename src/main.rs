1c21a231b7af6016ddbf7a5c0e368762e9f90fb9
ros <ros65536@gmail.com>
2018-10-26 16:49:21 +0100

    add command line parsing and default grid size

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
+use std::collections::HashMap;
+use std::env;
+
+const X_SIZE: &'static str = "20";
+const Y_SIZE: &'static str = "20";
+
+fn init_deafult_args() -> HashMap<String, String> {
+    let mut map = HashMap::new();
+    map.insert("xSize", X_SIZE);
+    map.insert("ySize", Y_SIZE);
+
+    map.iter().map(|p| (String::from(*p.0), String::from(*p.1))).collect()
+} 
+
+fn init_args() -> HashMap<String, String> {
+    let mut map = init_deafult_args();
+
+    let mut key = String::new();
+    for arg in env::args() {
+        if arg.starts_with("-") && arg.len() > 1 {
+            key = arg.splitn(2, "-").collect();
+        } else {
+            if !key.is_empty() {
+                map.insert(key.clone(), arg.clone());
+            } else {
+                println!("Invalid argument: {}", arg);
+            }
+
+            key.clear();
+        } 
+    }
+
+    map
+}
+
 fn main() {
-    let x = 2;
-    println!("Hello, world {}!", x);
+    let args = init_args();
+    println!("{:?}", args);
 }
 
